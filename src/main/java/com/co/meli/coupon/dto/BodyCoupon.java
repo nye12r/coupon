210f3fa75e5fe2666a4dfa8dbee530ec7269748e
Luis Eduardo Rosales Prieto <nye12r@gmail.com>
2022-04-16 23:27:33 -0500

    :fire: add lombok arguments

--- src/main/java/com/co/meli/coupon/dto/BodyCoupon.java
+++ src/main/java/com/co/meli/coupon/dto/BodyCoupon.java
@@ -3,9 +3,7 @@ package com.co.meli.coupon.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -14,12 +12,14 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BodyCoupon {
     @ApiModelProperty(value = "Lista de Items favoritos", required = true)
-    @JsonProperty("item_ids")
     @NotNull(message = "La Lista de Items no puede ser vacia")
     @NotEmpty(message = "La Lista de Items no puede ser vacia")
     @SerializedName("item_ids")
+    @JsonProperty("item_ids")
     private List<String> itemIDS;
 
     @ApiModelProperty(value = "Valor del cupon", required = true)
